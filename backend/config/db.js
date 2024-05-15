29c9b26b56ebd4d95980c3c5a50c57c663c136a9
Muneeshwar Mariappan <muneeshwar.mariappan@abstract-tech.com>
2024-05-16 01:31:30 +0530

    made changes as per react

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const color = require("colors");
-mongoose.set("strictQuery", true); // or false
+mongoose.set('strictQuery', true); // or false
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -9,11 +9,13 @@ const options = {
   //  maxPoolSize: 10, // Maintain up to 10 socket connections
   //  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   //  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4, // Use IPv4, skip trying IPv6
-};
+  family: 4 // Use IPv4, skip trying IPv6
+}
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.DB_HOST_URL, options);
+    const connection = await mongoose.connect(
+      process.env.DB_HOST_URL, options
+    );
     console.log(
       `Mongoose connected: ${connection.connection.host}`.cyan.underline
     );
